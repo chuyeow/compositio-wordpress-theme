2ec6dbfb5259a5dae671c7c2d6d953a2cd1f6d7f
Cheah Chu Yeow <chuyeow@gmail.com>
2009-07-02 14:54:27 +0800

    Fix indentation, and remove RSS link for comments.

--- compositio/sidebar.php
+++ compositio/sidebar.php
@@ -1,42 +1,38 @@
 <div class="SR">
 
-<!-- Start Search -->
 <div class="search">
   <form method="get" action="<?php bloginfo('url'); ?>/">
-   <fieldset>
-   <input type="text" value="<?php the_search_query(); ?>" name="s" /><button type="submit">Search</button>
-   </fieldset>
+    <fieldset>
+      <input type="text" value="<?php the_search_query(); ?>" name="s" /><button type="submit">Search</button>
+    </fieldset>
   </form>
-<div class="syn">
- <ul>
-  <li><a href="<?php bloginfo('rss2_url'); ?>">Entries</a> (RSS)</li>
-  <li><a href="<?php bloginfo('comments_rss2_url'); ?>">Comments</a> (RSS)</li>
- </ul>
-</div>
-</div>
-<!-- End Search -->
 
-<!-- Start About This Blog -->
-<div class="about">
-<h3>About this blog</h3>
-<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Quisque sed felis. Aliquam sit amet felis. Mauris semper, velit semper laoreet dictum, quam diam dictum urna, nec placerat elit nisl in quam. Etiam augue pede, molestie eget, rhoncus at, convallis ut, eros. Aliquam pharetra.</p>
+  <div class="syn">
+    <ul>
+      <li><a href="<?php bloginfo('rss2_url'); ?>">Entries</a> (RSS)</li>
+    </ul>
+  </div>
 </div>
-<!-- End About This Blog -->
+
+
+<div class="about">
+  <h3>About this blog</h3>
+  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Quisque sed felis. Aliquam sit amet felis. Mauris semper, velit semper laoreet dictum, quam diam dictum urna, nec placerat elit nisl in quam. Etiam augue pede, molestie eget, rhoncus at, convallis ut, eros. Aliquam pharetra.</p>
+</div>
 
 
 <div class="categs">
-  <div>
   <h3>Categories</h3>
-   <ul>
-    <?php wp_list_categories('show_count=1&title_li='); ?>
+  <ul>
+    <?php wp_list_categories('title_li='); ?>
   </ul>
-  </div>
-  <div style="margin-left: 10px;">
-   <h3>Archives</h3>
-    <ul>
-     <?php wp_get_archives('type=monthly'); ?>
-    </ul>
-  </div>
+</div>
+
+<div class="categs">
+  <h3>Archives</h3>
+  <ul>
+    <?php wp_get_archives('type=monthly'); ?>
+  </ul>
 </div>
 
 
