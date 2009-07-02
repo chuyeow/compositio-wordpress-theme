9d1117582e8e06445778b5123e53f08e047d6cc1
Cheah Chu Yeow <chuyeow@gmail.com>
2009-07-02 14:34:12 +0800

    Clean up the <head>.

--- compositio/header.php
+++ compositio/header.php
@@ -1,21 +1,25 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <? $theTitle=wp_title(" - ", false); if($theTitle != "") { ?><title><?php echo wp_title("",false); ?> - <?php bloginfo('name'); ?></title>
 <? } else { ?><title><?php bloginfo('name'); ?></title><? } ?>
-<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-<meta http-equiv="pragma" content="no-cache" />
-<meta http-equiv="cache-control" content="no-cache" />
+
+<meta name="author" content="Cheah Chu Yeow" />
+<meta name="Copyright" content="Copyright (c) 2003-<?php echo date('Y'); ?> Cheah Chu Yeow" />
+
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
-<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
 <link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
-<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico" type="image/x-icon" />
+
+<link rel="shortcut icon" href="<?php bloginfo('siteurl'); ?>/favicon.ico" type="image/x-icon" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
 <?php wp_head(); ?>
 </head>
+
 <body>
 
 <!-- Start BG -->
