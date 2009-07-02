3896322d2b3a8ac0c8f3fb7e9a4988c3c49b455a
Cheah Chu Yeow <chuyeow@gmail.com>
2009-07-02 23:26:25 +0800

    Add my Google Analytics tracking JS.

--- compositio/footer.php
+++ compositio/footer.php
@@ -2,7 +2,6 @@
 <?php get_sidebar(); ?>
 <!-- Container -->
 
-
 </div></div>
 <!-- End BG -->
 
@@ -11,5 +10,10 @@
 <p class="theme">Heavily modified Compositio Theme created by: <a href="http://designdisease.com/" title="Professional Blog Design">Design Disease</a> brought to you by <a href="http://premiumthemes.com/">PremiumThemes.com</a></p> 
 </div>
 
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
+<script type="text/javascript">
+_uacct = 'UA-4048515-1';
+urchinTracker();
+</script>
 </body>
 </html>
